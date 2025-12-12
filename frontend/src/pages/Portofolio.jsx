b3a11264d8dd9096d78b32eebde63a8eadd3d62b
mesinajake <mesinajake9@gmail.com>
2025-12-12 20:50:43 +0800

    Changes to be committed: 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/pages/Portofolio.jsx

--- frontend/src/pages/Portofolio.jsx
+++ frontend/src/pages/Portofolio.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import SwipeableViews from "react-swipeable-views";
+import SwipeableViews from "react-swipeable-views-core";
 import { useTheme } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Tabs from "@mui/material/Tabs";
